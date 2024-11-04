7d2f590b95b4a6b00be2f793f9e88b7cc7e4aa30
Hemanth Reddy <hemanthreddy@Hemanths-MacBook-Air.local>
2024-11-04 13:05:52 -0500

    fix build errors

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -115,7 +115,7 @@ export const SignInCard = ({setState }: SignInCardProps) => {
                 </Button>
             </div>
             <p className="text-xs text-muted-foreground">
-                Don't have an account? <span onClick ={()=>setState("signUp")}className="text-sky-700 hover:underline cursor-pointer"> Sign up</span>
+            Don&apos;t have an account?{" "} <span onClick ={()=>setState("signUp")}className="text-sky-700 hover:underline cursor-pointer"> Sign up</span>
             </p>
        
         </CardContent>
